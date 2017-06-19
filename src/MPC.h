7e238f7b337c899633903b34b51242c81673c504
eqbal <info@eqbalq.com>
2017-06-19 17:53:09 +0300

    Add Scale factors for total cost

--- src/MPC.h
+++ src/MPC.h
@@ -6,15 +6,39 @@
 
 using namespace std;
 
+const size_t N  = 20;
+const double dt = 0.05;
+const int Latency_dt = 2;
+
+// Scale factors for Cost
+const double SCALE_DELTA  = 1.0;
+const double SCALE_ACC    = 10.0;
+const double SCALE_DELTA_D = 300.0;
+const double SCALE_ACC_D  = 2.0;
+
+// Result from the Solver
+struct Resultant {
+    vector<double>  X;      // X coordinate
+    vector<double>  Y;      // Y coordinate
+    vector<double>  Psi;    // Orientation angle
+    vector<double>  V;      // Velocity
+    vector<double>  CTE;    // Cross Track Error
+    vector<double>  EPsi;   // Orientation angle Error
+    vector<double>  Delta;  // Steering angle
+    vector<double>  A;      // Acceleration (throttle)
+};
+
 class MPC {
  public:
   MPC();
 
   virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  Resultant Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+  double delta_previous {0};
+  double acc_previous {0.1};
+
 };
 
 #endif /* MPC_H */
